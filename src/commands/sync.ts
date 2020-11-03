37935c4f0f934af1ad8194ac354a65d47c64bb45
igorbezsmertnyii <mr.igor96@outlook.com>
2020-11-03 11:22:56 +0100

    Feature/implement sync functionality

---
+++ src/commands/sync.ts
@@ -0,0 +1,51 @@
+import {Command, flags} from '@oclif/command'
+import cli from 'cli-ux'
+import FileManager from '../services/file-manager.service'
+import SecretsManager from '../services/secrets-manager.service'
+import {Files} from '../constants'
+
+export default class Sync extends Command {
+  private fileManager = new FileManager()
+
+  private secretsManager = new SecretsManager()
+
+  static description = 'This command used to sync variable from local environment with AWS SecretsManager'
+
+  static examples = [
+    '$ sm-cli fetch',
+    '$ sm-cli fetch -p PROFILE_NAME',
+  ]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    // flag with a value (-p, --profile=VALUE)
+    profile: flags.string({
+      char: 'p',
+      description: 'AWS profile name',
+    }),
+  }
+
+  async run() {
+    const {flags} = this.parse(Sync)
+
+    if (flags.profile) {
+      this.secretsManager.changeProfile = flags.profile
+    }
+  
+    await cli.action.start('starting a process', 'initializing')
+
+    const envs = await this.fileManager.readFile(Files.Env)
+    const formattedEnvs = envs.split('\n').reduce((acc: Record<string, string>, line: string) => {
+      const [key, value] = line.split('=')
+      acc[key] = value.replace(/\'/g, '')
+      return acc
+    }, {})
+
+    const secrets = await this.secretsManager.syncSecrets(formattedEnvs)
+    const envsList = Object.entries(secrets).map(([key, value]) => `${key}='${value}'`).join('\n')
+
+    await this.fileManager.createFile(Files.Env, envsList)
+
+    await cli.action.stop('env\'s successfully synced')
+  }
+}
--- src/services/secrets-manager.service.ts
+++ src/services/secrets-manager.service.ts
@@ -1,6 +1,7 @@
 import * as AWS from 'aws-sdk'
 import FileManager from './file-manager.service'
 import {Config} from '../attributes'
+import {Files} from '../constants'
 
 class SecretsManager {
   private fileManager: FileManager
@@ -12,7 +13,17 @@ class SecretsManager {
   constructor() {
     this.credentials = new AWS.SharedIniFileCredentials()
     this.fileManager = new FileManager()
-    this.config = JSON.parse(this.fileManager.readFile('.secretsrc'))
+    this.config = JSON.parse(this.fileManager.readFile(Files.Secretsrc))
+  }
+
+  public async syncSecrets(secrets: Record<string, string>) {
+    await this.client.updateSecret({
+      SecretId: this.config.project,
+      SecretString: JSON.stringify(secrets)
+    }).promise()
+
+    const response = await this.fetchSecrets()
+    return response
   }
 
   public async fetchSecrets() {
